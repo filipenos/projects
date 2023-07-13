3ded25593231f81b27f915bd51bccb117bdb86ba
Filipe Peixoto <filipenos@gmail.com>
2023-07-13 05:31:11 -0300

    faet(all): ssh allow to execute cmd

--- pkg/command/exec.go
+++ pkg/command/exec.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/filipenos/projects/pkg/config"
+	"github.com/filipenos/projects/pkg/log"
 	"github.com/filipenos/projects/pkg/path"
 	"github.com/filipenos/projects/pkg/project"
 	"github.com/spf13/cobra"
@@ -32,29 +34,55 @@ func execCmd(cmdParam *cobra.Command, params []string) error {
 		return fmt.Errorf("project not found")
 
 	}
-	if p.Path == "" {
-		return fmt.Errorf("project '%s' dont have path", p.Name)
-	}
-	if !path.Exist(p.Path) {
-		return fmt.Errorf("path '%s' of project '%s' not exists", p.Path, p.Name)
+	if err := p.Validate(); err != nil {
+		return err
 	}
 	if len(params) < 2 {
 		return fmt.Errorf("required at least one argument")
 	}
 
+	//vscode-remote://ssh-remote+filipe-pc/home/filipe/Code/Digipix/papirus
+
 	var (
 		toExec = params[1]
 		args   []string
+		path   string
 	)
+
+	switch p.ProjectType {
+	case project.ProjectTypeLocal:
+		path = p.Path
+	case project.ProjectTypeSSH:
+		log.Infof("executing on ssh host, some features not working")
+		i := strings.Index(p.Path, "+")
+		if i == -1 {
+			return fmt.Errorf("invalid path")
+		}
+		subPath := p.Path[i+1:]
+		i = strings.Index(subPath, "/")
+		if i == -1 {
+			return fmt.Errorf("invalid path")
+		}
+		sshHost := subPath[:i]
+		subPath = subPath[i:]
+
+		args = append(args, sshHost)
+		args = append(args, "-t") // Force pseudo-tty allocation
+		args = append(args, "cd "+subPath+";"+toExec)
+		toExec = "ssh"
+	}
+
 	if len(params) > 2 {
-		args = params[2:]
+		args = append(args, params[2:]...)
 	}
 
 	cmd := exec.Command(toExec, args...)
-	cmd.Dir = p.Path
+	cmd.Dir = path
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Run()
+	if err := cmd.Run(); err != nil {
+		return err
+	}
 	return nil
 }
