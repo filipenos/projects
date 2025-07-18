0d598fdb852e3a4b22468ba73482d048cc28bfb2
Filipe Peixoto <filipenos@gmail.com>
2025-07-18 13:16:47 -0300

    feat: enhance editor command with dynamic editor support
    - Introduced an editor service that loads configuration and initializes available editors.
    - Updated the `code` command to utilize the new editor service for opening projects.
    - Removed hardcoded editor logic in favor of a more flexible approach using editor aliases.

--- pkg/command/code.go
+++ pkg/command/code.go
@@ -1,22 +1,33 @@
+// command/code.go
 package command
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
 
-	"github.com/filipenos/projects/pkg/log"
+	"github.com/filipenos/projects/pkg/config"
+	"github.com/filipenos/projects/pkg/editor"
 	"github.com/filipenos/projects/pkg/path"
 	"github.com/filipenos/projects/pkg/project"
-	"github.com/filipenos/projects/pkg/workspace"
 	"github.com/spf13/cobra"
 )
 
+var editorService *editor.Service
+
 func init() {
+	// Carrega a configuração
+	cfg := config.Load()
+
+	// Inicializa o serviço de editores
+	var err error
+	editorService, err = editor.NewService(cfg)
+	if err != nil {
+		fmt.Printf("Warning: failed to initialize editor service: %v\n", err)
+	}
+
 	codeCmd := &cobra.Command{
 		Use:     "code",
 		Short:   fmt.Sprintf("Edit your project using the editor (%s as default)", cfg.Editor),
-		Aliases: []string{"vscode", "subl", "zed", "nvim"},
+		Aliases: editorService.GetAvailableEditors(),
 		RunE:    code,
 	}
 	codeCmd.Flags().StringP("window", "w", "new", "Window type (new|reuse|add)")
@@ -32,108 +43,27 @@ func code(cmdParam *cobra.Command, params []string) error {
 	p, _ := projects.Find(path.SafeName(params...))
 	if p == nil {
 		return fmt.Errorf("project not found")
-
 	}
+
 	if err := p.Validate(); err != nil {
 		return err
 	}
 
-	args := make([]string, 0)
+	// Converte a flag window para o tipo apropriado
+	windowFlag := SafeStringFlag(cmdParam, "window")
+	var window editor.WindowType
 
-	var editor string
-	window := SafeStringFlag(cmdParam, "window")
-	openType := "folder"
-
-	switch cmdParam.CalledAs() {
-	case "code", "vscode":
-		editor = "code"
-
-		switch window {
-		case "reuse":
-			args = append(args, "--reuse-window")
-		case "add":
-			args = append(args, "--add")
-		default:
-			args = append(args, "--new-window")
-		}
-
-		if p.IsWorkspace {
-			args = append(args, "--file-uri")
-			openType = "file"
-		} else {
-			args = append(args, "--folder-uri")
-		}
-		args = append(args, p.RootPath)
-
-	case "subl", "sublime":
-		editor = "subl"
-
-		if p.ProjectType != project.ProjectTypeLocal {
-			return fmt.Errorf("sublime not support remote project")
-		}
-
-		switch window {
-		case "new":
-			args = append(args, "--new-window")
-		case "add":
-			args = append(args, "--add")
-			//default reuse window
-		}
-
-		if p.IsWorkspace {
-			w, err := workspace.Load(p.RootPath)
-			if err != nil {
-				return err
-			}
-			args = append(args, w.FoldersPath()...)
-		} else {
-			args = append(args, p.RootPath)
-		}
-
-	case "zed":
-		editor = "zed"
-
-		if p.ProjectType != project.ProjectTypeLocal {
-			return fmt.Errorf("zed not support remote project")
-		}
-
-		switch window {
-		case "add":
-			args = append(args, "--add")
-			//default new window
-		}
-
-		if p.IsWorkspace {
-			w, err := workspace.Load(p.RootPath)
-			if err != nil {
-				return err
-			}
-			args = append(args, w.FoldersPath()...)
-		} else {
-			args = append(args, p.RootPath)
-		}
-
-	case "nvim":
-		editor = "nvim"
-
-		switch p.ProjectType {
-		case project.ProjectTypeLocal, project.ProjectTypeWSL:
-		default:
-			return fmt.Errorf("project type %s not supported", p.ProjectType)
-		}
-
-		args = append(args, p.Path)
-
+	switch windowFlag {
+	case "reuse":
+		window = editor.WindowTypeReuse
+	case "add":
+		window = editor.WindowTypeAdd
 	default:
-		return fmt.Errorf("editor not supported")
+		window = editor.WindowTypeNew
 	}
 
-	log.Infof("open %s '%s' on '%s'", openType, p.RootPath, editor)
-
-	cmd := exec.Command(editor, args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
-
+	// Usa o serviço de editores para abrir o projeto
+	return editorService.OpenProject(cmdParam.CalledAs(), p, window)
 }
+
+// Comandos auxiliares para gerenciar editores
