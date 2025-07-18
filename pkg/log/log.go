0147b60c9035e41845a1895029f775d0c249a8fe
Filipe Peixoto <filipenos@gmail.com>
2025-07-18 13:13:50 -0300

    feat(log): add Warnf function for logging warnings
    This commit introduces a new Warnf function in the log package.
    The Warnf function formats and prints warning messages to the console,
    allowing for better visibility of potential issues during execution.

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -5,3 +5,7 @@ import "fmt"
 func Infof(msg string, args ...any) {
 	fmt.Printf("[projects] %s\n", fmt.Sprintf(msg, args...))
 }
+
+func Warnf(msg string, args ...any) {
+	fmt.Printf("[projects] WARNING: %s\n", fmt.Sprintf(msg, args...))
+}
